b077187c6d775e1252e368c65aae24adcc9bd8db
Victor Alves <vsilva.programador@gmail.com>
2018-04-20 17:56:25 -0300

    Update oauth2/index.js

--- oauth2/index.js
+++ oauth2/index.js
@@ -3,7 +3,7 @@ const server = require('./server');
 const Strategies = require('./strategies');
 
 module.exports.token = [
-	passport.authenticate(['basic', 'oauth2-client-password'], { session: false }),
-	server.token(),
-	server.errorHandler()
+  passport.authenticate(['basic', 'oauth2-client-password'], { session: false }),
+  server.token(),
+  server.errorHandler()
 ];
